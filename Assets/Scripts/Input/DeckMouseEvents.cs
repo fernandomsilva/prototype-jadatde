ac5f3159ba4684ae98c93e5d4a7f646acaf907a5
fernandomsilva <fms2005@gmail.com>
2020-10-20 09:56:18 -0700

    Adicionando a mecanica de comprar cartas e iniciando o script de atributos das cartas

--- Assets/Scripts/Input/DeckMouseEvents.cs
+++ Assets/Scripts/Input/DeckMouseEvents.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class DeckMouseEvents : MonoBehaviour
 {
+	public GameObject cards;
+	
+	private CardStats[] cardSlots;
+	private int maxHandSize;
+	
+	private string[] deck = {"Summon Footman", "Throw Fireball", "Freeze Enemies"};
+	
     // Start is called before the first frame update
     void Start()
     {
-        
+        cardSlots = cards.GetComponentsInChildren<CardStats>();
+		maxHandSize = 5;
+		
+		//Debug.Log(NumberOfCardsInHand());
     }
+	
+	int NumberOfCardsInHand()
+	{
+		int count = 0;
+		
+		foreach (CardStats card in cardSlots)
+		{
+			if (card.cardName != "")
+			{
+				count += 1;
+			}
+			else
+			{
+				break;
+			}
+		}
+		
+		return count;
+	}
 	
 	void OnMouseDown()
 	{
-		Debug.Log("Coracao das cartas");
+		int handLength = NumberOfCardsInHand();
+		
+		if (handLength < maxHandSize)
+		{
+			int randomValue = Random.Range(0, 3);
+			
+			cardSlots[handLength].loadCard(deck[randomValue]);
+		}
 	}
 
     // Update is called once per frame
---
+++ Assets/Scripts/Stats/CardStats.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardStats : MonoBehaviour
+{
+	public CardDatabase cardDatabase;
+	
+	public string cardName;
+	public string type;
+	public string element;
+	public int HP;
+	public int attack;
+	public int attackSpeed;
+	public int moveSpeed;
+	public int amount;
+	public int damage;
+	public int area;
+	public int cost;
+	
+	private SpriteRenderer mySpriteRenderer;
+	
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+		
+    }
+
+	public void loadCard(string newName)
+	{
+		cardName = newName;
+		loadCardImage();
+	}
+	
+	void loadCardImage()
+	{
+		mySpriteRenderer.sprite = cardDatabase.GetCardSprite(cardName);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
