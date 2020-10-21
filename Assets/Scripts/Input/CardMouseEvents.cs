ecb921e17d9bf8b2104a91a8ad7b3eacb10ae7a7
fernandomsilva <fms2005@gmail.com>
2020-10-21 14:10:39 -0700

    Mostrando a quantidade de MP a ser gasto ao invocar a carta selecionada

--- Assets/Scripts/Input/CardMouseEvents.cs
+++ Assets/Scripts/Input/CardMouseEvents.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CardMouseEvents : MonoBehaviour
 {
+	private PlayerStats playerStatsScript;
+	private BackgroundMouseFlag backgroundMouseFlag;
+	
 	private bool selected;
 	private Vector3 mousePositionLastFrame;
 	private Vector3 myHUDPosition;
 	
+	private CardStats myCardStats;
+	
     // Start is called before the first frame update
     void Start()
     {
+		playerStatsScript = GameObject.Find("Player").GetComponent<PlayerStats>();
+		backgroundMouseFlag = GameObject.Find("Field Background").GetComponent<BackgroundMouseFlag>();
+		
         selected = false;
 		myHUDPosition = transform.position;
+		
+		myCardStats = GetComponent<CardStats>();
     }
 	
 	void OnMouseUp()
@@ -20,14 +30,18 @@ public class CardMouseEvents : MonoBehaviour
 		if (selected)
 		{
 			selected = false;
+			//backgroundMouseFlag.mouseIsOver = false;
 			
 			transform.position = myHUDPosition;
+			
+			playerStatsScript.hideManaToSpend();
 		}
 	}
 	
 	void OnMouseDown()
 	{
 		selected = true;
+		//backgroundMouseFlag.mouseIsOver = false;
 		
 		mousePositionLastFrame = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 	}
@@ -36,11 +50,19 @@ public class CardMouseEvents : MonoBehaviour
 	{
 		if (selected)
 		{
-			Vector3 moveDistance = Camera.main.ScreenToWorldPoint(Input.mousePosition) - mousePositionLastFrame;
+			Vector3 currentMouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector3 moveDistance = currentMouseWorldPosition - mousePositionLastFrame;
 			
-			transform.position += moveDistance;
+			//currentMouseWorldPosition.z = 0.0f;
 			
+			transform.position += moveDistance;			
 			mousePositionLastFrame = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			
+			playerStatsScript.displayManaToSpend(myCardStats.cost);
+			
+			if (backgroundMouseFlag.mouseIsOver)
+			{
+			}
 		}
 	}
 
